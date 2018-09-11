d528ef25851e55d2b6f6a020eaad53b4ce3d0db1
loena.kaljouw@student.hu.nl <p0k3m0n12>
2018-09-11 10:36:12 +0200

    Backup

--- School/Les7/E_4.py
+++ School/Les7/E_4.py
@@ -1,8 +1,10 @@
 import datetime
-with open("hardlopers.txt", "a+") as file:
-    naam = input("Naam van de hardloper: ")
-    vandaag = datetime.datetime.today()
-    info = vandaag.strftime("%a %d %b %Y" + "," +" %H" + ":" + "%M" + ":" + "%S" + ", ")
-    print(info + naam)
-    file.writelines(info + naam + "\n")
-    file.close()
+x=1
+while x!=0:
+    with open("hardlopers.txt", "a+") as file:
+        naam = input("Naam van de hardloper: ")
+        vandaag = datetime.datetime.today()
+        info = vandaag.strftime("%a %d %b %Y" + "," +" %H" + ":" + "%M" + ":" + "%S" + ", ")
+        print(info + naam)
+        file.writelines(info + naam + "\n")
+        file.close()
