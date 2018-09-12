8a78fb1c5be467dc8edae914ae158477b67665fd
loena.kaljouw@student.hu.nl <p0k3m0n12>
2018-09-12 20:19:46 +0200

    Backup

--- School/Les8/E_Final.py
+++ School/Les8/E_Final.py
@@ -2,6 +2,7 @@ aantalkluizen = 12
 vrijekluizen = []
 bezettekluizen = []
 bezet=0
+save = []
 def toon_aantal_kluizen_vrij():
     return len(vrijekluizen)
 def nieuwe_kluis():
@@ -45,10 +46,22 @@ def kluis_teruggeven():
                 tekstcheck = (str(tekst).split(";"))
                 codecontrole = tekstcheck[1].replace("\n","")
                 if codecontrole == codecheck:
-                    return "Je kluis is open"
+                    save=[]
+                    with open("kluizen.txt", "r+") as file:
+                        for te in file.readlines():
+                            if te != tekst:
+                                save.append(te)
+                            with open("kluizen.txt", "w+") as file:
+                                for a in save:
+                                    file.write(a)
+                            file.close()
+                            print(bezettekluizen)
+                            print(vrijekluizen)
+                        file.close()
+                    file.close()
+                    return "Je kluis is nu gereset"
                 return "Foute code"
     return "Dit kluisnummmer bestaat niet"
-
 
 x = 1
 while x > 0:
@@ -65,6 +78,8 @@ while x > 0:
             else:
                 vrijekluizen.append(kluizen)
         file.close()
+    print(vrijekluizen)
+    print(bezettekluizen)
     print("1: Ik wil weten hoeveel kluizen nog vrij zijn" + "\n" + "2: Ik wil een nieuwe kluis" + "\n" + "3: Ik wil even iets uit mijn kluis halen" + "\n" + "4: Ik geef mijn kluis terug" + "\n" + "5: Ik wil stoppen")
     keuze = input("Ik kies optie: ")
     if int(keuze) > 5:
